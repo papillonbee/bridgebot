92ea90492a87d6fcb8ee341a6e773ca04a72d238
Papan Yongmalwong <papan.yongmalwong@seamoney.com>
2025-04-07 02:23:42 +0800

    upgrade to v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bridgebot",
-    version="0.0.1",
+    version="0.0.2",
     author="Papan Yongmalwong",
     author_email="papillonbee@gmail.com",
     description="bridgebot is a python package for building floating bridge bot!",
